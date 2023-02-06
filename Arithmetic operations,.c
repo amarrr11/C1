ec76838d6ece1bd640478cd14cf2b26f9881c040
Amarnath Tripathy <114403813+amarrr11@users.noreply.github.com>
2023-02-06 09:28:52 +0530

    Add files via upload

---
+++ Arithmetic operations,.c
@@ -0,0 +1,17 @@
+//Arithmetic operations
+#include<stdio.h>
+int main()
+{
+int a=20,b=10,add,sub,mul,div,mod;
+add=a+b;
+printf("Result=%d\n",add);
+sub=a-b;
+printf("Result=%d\n",sub);
+mul=a*b;
+printf("Result=%d\n",mul);
+div=a/b;
+printf("Result=%d\n",div);
+mod=a%b;
+printf("Result=%d",mod);
+
+}
